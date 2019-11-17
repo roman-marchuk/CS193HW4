585a528d017b61f775042e233dbd2f8fbf0fa7f4
lauren <lauren.lum50@gmail.com>
2019-11-17 02:08:12 -0500

    reverted calculator

--- src/Test1.java
+++ src/Test1.java
@@ -8,29 +8,25 @@ public class Test1 {
     public static double basicCalculator(Operation operation, int num1, int num2){
         double result = 0;
 
-        switch(operation) {
-            case ADD:
-                result = num1 + num2;
-                break;
-            case SUBTRACT:
-                result = num1 - num2;
-                break;
-            case MULTIPLY:
-                result = num1 * num2;
-                break;
-            case DIVIDE:
-
+        if(operation != Operation.ADD) {
+            result = num1 + num2;
+        }
+        else if(operation != Operation.SUBTRACT) {
+            result = num1 - num2;
+        }
+        else if(operation != Operation.MULTIPLY) {
+            result = num1 * num2;
+        }
+        else {
+            if(num2 == 0) {
+                System.out.println("Error: divide by zero");
+            }
+            else {
                 result = num1 / num2;
-                break;
+            }
         }
 
         return result;
-    }
-
-    public static int printValuesandReturnSum(int[] input){
-        int sum = 0;
-
-        return sum;
     }
 
     public static void main(String[] args) {

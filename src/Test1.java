4541ab65e09cab28168eeac219fa0adb205f10f3
lauren <lauren.lum50@gmail.com>
2019-11-17 01:15:41 -0500

    added calculator with if statements

--- src/Test1.java
+++ src/Test1.java
@@ -8,6 +8,24 @@ public class Test1 {
     public static double basicCalculator(Operation operation, int num1, int num2){
         double result = 0;
 
+        if(operation == Operation.ADD) {
+            result = num1 + num2;
+        }
+        else if(operation == Operation.SUBTRACT) {
+            result = num1 - num2;
+        }
+        else if(operation == Operation.MULTIPLY) {
+            result = num1 * num2;
+        }
+        else {
+            if(num2 == 0) {
+                System.out.println("Error: divide by zero");
+            }
+            else {
+                result = num1 / num2;
+            }
+        }
+
         return result;
     }
 
@@ -18,6 +36,6 @@ public class Test1 {
     }
 
     public static void main(String[] args) {
-        System.out.println(basicCalculator(Operation.ADD, 1, 2));
+        System.out.println(basicCalculator(Operation.DIVIDE, 1, 2));
     }
 }

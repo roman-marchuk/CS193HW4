bacc64e20bf5e3297cac90f468c58c02f06f502f
sarahmthomas <42981898+sarahmthomas@users.noreply.github.com>
2020-06-30 23:10:31 -0700

    Fall 2020 Updates (#1)
    * replace if-else example with case switch
    
    * update README.md for fall 2020 with if-else version
    
    * FIX typecast error
    
    * ADD rubric to README
    
    Co-authored-by: sarahmthomas <sarahthomas.maria@gmail.com>

--- src/Test1.java
+++ src/Test1.java
@@ -1,35 +1,37 @@
 import java.util.*;
+
 enum Operation
 {
     ADD, SUBTRACT, MULTIPLY, DIVIDE
 }
 public class Test1 {
 
-    public static double basicCalculator(Operation operation, int num1, int num2){
+    public static double basicCalculator(final Operation operation, final int num1, final int num2){
         double result = 0;
 
-        if(operation != Operation.ADD) {
-            result = num1 + num2;
+        switch(operation) {
+            case ADD:
+                // if operation = ADD, return the sum
+                result = num1 + num2;
+                break;
+            case SUBTRACT:
+                result = num1 - num2;
+                break;
+            case MULTIPLY:
+                result = num1 * num2;
+                break;
+            case DIVIDE:
+                if (num2 == 0) {
+                    System.out.println("Can't divide by 0");
+                } else {
+                    result = ((double)num1) / num2;
+                }
+                break;
         }
-        else if(operation != Operation.SUBTRACT) {
-            result = num1 - num2;
-        }
-        else if(operation != Operation.MULTIPLY) {
-            result = num1 * num2;
-        }
-        else {
-            if(num2 == 0) {
-                System.out.println("Error: divide by zero");
-            }
-            else {
-                result = num1 / num2;
-            }
-        }
-
         return result;
     }
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         System.out.println(basicCalculator(Operation.DIVIDE, 1, 2));
     }
 }
